891b2d8e69654c9ada6519384287dcc51be3810e
Richard Gama <rgamag128@gmail.com>
2020-11-08 23:27:52 -0600

    Test DB

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 require("dotenv").config({ path: ".env" });
-const BBDD = process.env.BBDD;
+//const BBDD = process.env.BBDD;
+const BBDD = 'mongodb+srv://Gamasoft:Gamasoft@clustercms.gbsxk.mongodb.net/cmsKMM?retryWrites=true&w=majority';
 
 mongoose.connect(BBDD,{
     useCreateIndex:true,
     useNewUrlParser:true,
     useFindAndModify:false
 }).then( db => console.log('DB is conected'))
-  .catch(err => console.error());
+  .catch(err => 'Error to connect DB');
